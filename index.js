e25037f5e260749cdb58fa9e6b61f8010a69037b
Ken Stanley <ken.stanley@uncc.edu>
2022-04-26 23:24:51 -0400

    Add stubs for the azure endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const HTTP_PORT = process.env.HTTP_PORT;
 const app = express();
 app.disable('x-powered-by');
 
+app.use('/', require('./src/routes/routesAzure'));
 app.use('/docs', require('./src/routes/swagger'));
 
 app.listen(HTTP_PORT, () => console.log(`Server is running on port ${HTTP_PORT}`));
---
+++ src/routes/routesAzure.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const routes = express.Router();
+
+/**
+ * @swagger
+ * /entities:
+ *   post:
+ *     tags:
+ *      - "Azure Cognitive Service for Language"
+ *     description: "Identify different entities in text and categorize them into pre-defined types."
+ *     responses:
+ *       200:
+ *         description: "Returns a list of entities categorized into pre-defined classes or types such as\\: person, location, event, product, and organization."
+ *       400:
+ *         description: "An error describing any problems detected in the request."
+ */
+routes.post('/entities', (req, res) => {});
+
+/**
+ * @swagger
+ * /phrases:
+ *   post:
+ *     tags:
+ *      - "Azure Cognitive Service for Language"
+ *     description: "Identify the most important points in a piece of text."
+ *     responses:
+ *       200:
+ *         description: "Returns a list of key phrases for the given document."
+ *       400:
+ *         description: "An error describing any problems detected in the request."
+ */
+routes.post('/phrases', (req, res) => {});
+
+/**
+ * @swagger
+ * /sentiment:
+ *   post:
+ *     tags:
+ *      - "Azure Cognitive Service for Language"
+ *     description: "Detect positive, negative and neutral sentiment in text. Get more insights by mining opinions."
+ *     responses:
+ *       200:
+ *         description: "Returns structures describing the sentiment labels (positive, neutral, and negative) and confidence scores for the given document."
+ *       400:
+ *         description: "An error describing any problems detected in the request."
+ */
+routes.post('/sentiment', (req, res) => {});
+
+/**
+ * @swagger
+ * tags:
+ *   - name: "Azure Cognitive Service for Language"
+ *     description: "Understand conversations and unstructured text"
+ */
+module.exports = routes;
