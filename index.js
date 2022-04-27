e9184f1ec5c21152ea3d51281699cbd999b5d9c7
Ken Stanley <ken.stanley@uncc.edu>
2022-04-26 23:23:20 -0400

    Move HTTP_POST variable up

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const swaggerRoutes = require('./src/routes/swagger');
+const HTTP_PORT = process.env.HTTP_PORT;
 
 const app = express();
-const HTTP_PORT = process.env.HTTP_PORT;
 
 app.use('/docs', swaggerRoutes);
 app.listen(HTTP_PORT, () => console.log(`Server is running on port ${HTTP_PORT}`));
