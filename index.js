a1e488d8365ab5040a60143ef5c93590b72ffcb7
Ken Stanley <ken.stanley@uncc.edu>
2022-04-30 21:33:14 -0400

    Add a global error handler

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ app.use(express.json());
 app.use('/', require('./src/routes/routesAzure'));
 app.get('/', (req, res) => res.redirect('/docs'));
 app.use('/docs', require('./src/routes/swagger'));
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({error: err});
+});
 
 app.listen(HTTP_PORT, () => console.log(`Server is running on port ${HTTP_PORT}`));
