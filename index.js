59410e46d5b6e7dddf66fc3f1470a6c7d7a4a324
Ken Stanley <ken.stanley@uncc.edu>
2022-04-30 21:32:47 -0400

    Redirect get requests to the docs page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.disable('x-powered-by');
 
 app.use('/', require('./src/routes/routesAzure'));
+app.get('/', (req, res) => res.redirect('/docs'));
 app.use('/docs', require('./src/routes/swagger'));
 
 app.listen(HTTP_PORT, () => console.log(`Server is running on port ${HTTP_PORT}`));
