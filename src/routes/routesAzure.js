47f29bff200ad5d8a402496e6b71420a5fe8f1b9
Ken Stanley <ken.stanley@uncc.edu>
2022-04-30 22:20:08 -0400

    Update the swagger docs for the other two endpoints

--- src/routes/routesAzure.js
+++ src/routes/routesAzure.js
@@ -96,10 +96,29 @@ routes.post(
  *   post:
  *     tags:
  *      - "Azure Cognitive Service for Language"
- *     description: "Identify the most important points in a piece of text."
+ *     description: "Evaluate unstructured text, and return a list of the key phrases."
+ *     produces:
+ *      - "application/json"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                text:
+ *                  type: string
+ *                language:
+ *                  type: string
+ *            examples:
+ *               "application/json":
+ *                   summary: "Example to Analyze"
+ *                   value:
+ *                       text: "Thor's retirement is interrupted by a galactic killer known as Gorr the God Butcher, who seeks the extinction of the gods. To combat the threat, Thor enlists the help of King Valkyrie, Korg and ex-girlfriend Jane Foster, who - to Thor's surprise - inexplicably wields his magical hammer, Mjolnir, as the Mighty Thor. Together, they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late."
+ *                       lang: "en"
  *     responses:
  *       200:
- *         description: "Returns a list of key phrases for the given document."
+ *         description: "Returns a list of the key phrases for the given text."
  *       400:
  *         description: "An error describing any problems detected in the request."
  */
@@ -111,10 +130,36 @@ routes.post('/phrases', (req, res) => {});
  *   post:
  *     tags:
  *      - "Azure Cognitive Service for Language"
- *     description: "Detect positive, negative and neutral sentiment in text. Get more insights by mining opinions."
+ *     description: "Provide sentiment labels (such as \"negative\", \"neutral\" and \"positive\") and confidence scores at the sentence and document-level."
+ *     produces:
+ *      - "application/json"
+ *     parameters:
+ *      - name: opinions
+ *        in: query
+ *        description: "Provide more granular information about the opinions related to words (such as the attributes of products or services) in text."
+ *        schema:
+ *          type: boolean
+ *          default: false
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                text:
+ *                  type: string
+ *                language:
+ *                  type: string
+ *            examples:
+ *               "application/json":
+ *                   summary: "Example to Analyze"
+ *                   value:
+ *                       text: "Thor's retirement is interrupted by a galactic killer known as Gorr the God Butcher, who seeks the extinction of the gods. To combat the threat, Thor enlists the help of King Valkyrie, Korg and ex-girlfriend Jane Foster, who - to Thor's surprise - inexplicably wields his magical hammer, Mjolnir, as the Mighty Thor. Together, they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher's vengeance and stop him before it's too late."
+ *                       lang: "en"
  *     responses:
  *       200:
- *         description: "Returns structures describing the sentiment labels (positive, neutral, and negative) and confidence scores for the given document."
+ *         description: "Return confidence scores between 0 and 1 for each document & sentences within it for positive, neutral and negative sentiment."
  *       400:
  *         description: "An error describing any problems detected in the request."
  */
